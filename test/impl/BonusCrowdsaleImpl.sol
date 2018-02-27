500a4eba9a81cbff36e1a8cf9756ade16fa0df2e
BoutsPro <36363381+boutspro@users.noreply.github.com>
2018-02-27 16:05:13 +0530

    Create BonusCrowdsaleImpl.sol

---
+++ test/impl/BonusCrowdsaleImpl.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.11;
+
+//import "../../contracts/zeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "../../contracts/BonusCrowdsale.sol";
+
+
+contract BonusCrowdsaleImplToken is MintableToken {
+
+    // Metadata
+    string public constant symbol = "TMP";
+    string public constant name = "Test token";
+    uint8 public constant decimals = 18;
+    string public constant version = "1.0";
+
+}
+
+contract BonusCrowdsaleImpl is Crowdsale, BonusCrowdsale {
+
+    function BonusCrowdsaleImpl(
+        uint256 _startTime,
+        uint256 _endTime,
+        uint256 _rate,
+        address _wallet
+    )
+        Crowdsale(_startTime, _endTime, _rate, _wallet)
+        BonusCrowdsale(10) // $0.10
+    {
+    }
+
+    function createTokenContract() internal returns(MintableToken) {
+        return new BonusCrowdsaleImplToken();
+    }
+
+}
